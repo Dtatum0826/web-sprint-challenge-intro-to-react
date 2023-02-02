cd0512f927bc4174e582201b71ad1300f56708ab
Dustin Tatum <dtatum0826@gmail.com>
2023-02-02 14:09:11 -0600

    Completed MVP

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
-// Write your Character component here
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const CharacterContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
+const Accordion = styled.div`
+  border-radius:10px;
+`;
+
+const Character = ({ character }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <CharacterContainer>
+      <h1>Name: {character.name}</h1>
+      <button onClick={() => setIsOpen(!isOpen)}>Character Description</button>
+      {isOpen && (
+        <Accordion>
+          <p>Height: {character.height} cm</p>
+          <p>Weight: {character.mass} kg</p>
+          <p>Hair Color: {character.hair_color}</p>
+          <p>Eye Color: {character.eye_color}</p>
+        </Accordion>
+      )}
+    </CharacterContainer>
+  );
+};
+
+export default Character;
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Character from './components/Character';
+import Character from './Character';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     axios.get('https://swapi.dev/api/people/')
-      .then(response => 
-        
-      setCharacters(response.data));
+      .then(response => setCharacters(response.data.results));
   }, []);
 
   return (
